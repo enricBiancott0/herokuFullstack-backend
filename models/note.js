15598ae47deb26fe01a92f1b49e884b093dc3bbf
Enrico Biancotto <enrico.biancotto@studenti.unipd.it>
2022-07-14 20:43:50 +0200

    Update linter

--- models/note.js
+++ models/note.js
@@ -5,7 +5,7 @@ const url = process.env.MONGODB_URI
 console.log('connecting to MongoDb')
 
 mongoose.connect(url)
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
